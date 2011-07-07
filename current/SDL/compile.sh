e1347a0acd89abbd289d33433b9c54eae5bdf951
Stitch <Aquertu@stitch-open-source.googlecode.com>
2011-07-07 08:44:52 -0400

    Just reorganizing the Repo a bit

--- current/SDL/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ main.cpp lua.cpp init.cpp event.cpp loop.cpp render.cpp cleanup.cpp psurface.cpp -o pong -Wall -lSDL -llua
--- current/lua_c/fork/sdluac/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Compile Script: coreluac
-echo "Compiling...."
-g++ main.cpp -o coreluac -llua -Wall -lSDL
-echo "Finished Compiling coreluac..."
-echo "To invoke type ./coreluac -h...\n"
-
